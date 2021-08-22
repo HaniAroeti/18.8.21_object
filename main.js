44b594f4b4360d022bbe91a2247d985a65494800
HaniAroeti <haniaroeti@gmail.com>
2021-08-22 12:31:26 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+// Math.floor(1.5)
+// console.log(Math.floor(1.5));
+
+// Math.floor(Math.random()*10)
+// console.log(Math.floor(Math.random()*10));
+
+// Math.floor(0.9999*10)
+// console.log(Math.floor(0.9999*10));
+
+// Math.floor(0.9999*10)
+// console.log(Math.floor(0.9999*100));
+
+// Math.floor(0.9999*10)
+// console.log(Math.floor(0.9999*1000));
+
+// myDate = new Date();
+// console.log(myDate);
+
+// // myDate =  Date();
+// // console.log(myDate);
+
+// var myDate= myDate.getDate()
+// console.log(myDate);
+
+// תרגילי אוביקט  משתמש:
+// 1. צרו פונקציה שמקבלת מהמשתמש שם וגיל ץ הפונקציה מדפיסה ללוג אוביקט של PERSON
+// עם השדות
+
+// let inputName = prompt ("pls enter yor Name");
+// let inputAage = prompt("pls enter yor Age");
+
+// function prinPerson(personName,personAge){
+//     let person = {};
+//     person.name = personName;
+//     person.age = personAge;
+//     console.log(person);
+// }
+//     prinPerson(inputName,inputAage )
+
+
+// 2. צרו פונקציה שמקבלת מהמשתמש שם פרטי, שם משפחה וגיל
+// הפונקציה מדפיסה ללוג אוביקט של person
+// עם השדדות והערכים שהתקבלו מהמשתמש
+
+// let name1 = prompt("pls enter your name");
+// let lastName1 = prompt ("pls enter your last name");
+// let age1 = prompt (Number("please enter your age"));
+
+// function printNameLastNameAndAge (firstName, lastName, age){
+//     person={};
+//     person.name = firstName;
+//     person.lastName = lastName;
+//     person.myAge = age;
+//     console.log(person);
+// }
+// printNameLastNameAndAge(name1, lastName1, age1)
+
+// 3. צרו פונקציה שמקבלת מהמשתמש שם פרטי שם משפחה וגיל
+// הפונקציה מדפיסה ללוג אביקט של PERSON 
+// עם השדות שם מלא, גיל ואימיל שמורכב מהשם הפרטי+ 
+// @jmail.com
+// ןהערכים שהתקבלו מהמשתמש
+
+// let name1 = //prompt("pls enter your name");
+// let lastName1 = //prompt ("pls enter your last name");
+// // let age1 = prompt (Number("please enter your age"));
+// // function printPerson(fullName, age, email){
+//     var person = {};
+//     person.name = fullName;
+//     person.age = age;
+//     person.email = email;
+//     // console.log(person);
+// }
+// printPerson (name1,lastName1,age1)
+
+
+
+
+
+
+
+
+
+// 5. צרו פונקציה שמקבלת מהמשתמש שם רכב, חברה וסמק
+// הפונקציה מחזירה לי אובקט של CAR
+// עם השדות שם הרכב, חברה סמק שנת ייצור
+// (השנהשהאוביק CAR נוצר)
+// עשינו בכיתה
+// function getCar(carName,carBrand,carCapicity){
+//     var car = {};
+//     car.name = carName;
+//     car.brand = carBrand;
+//     car.capacity = carCapicity;
+//     var myDate = new Date();
+//     car.year = myDate.getFullYear();
+//     console.log(car);
+//     return car;
+// }
+//     var car_name = prompt("enter car name");
+//     var brand_name = prompt("enter brand name");
+//     var car_capacity = prompt ("enter car capacity")
+
+//     getCar (car_name,brand_name,car_capacity)
+
+
+
